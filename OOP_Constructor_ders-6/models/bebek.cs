c761a8bd64d2d7169d3a9bb0176618b541d66f38
byigitelmaci <142091808+byigitelmaci@users.noreply.github.com>
2023-09-08 23:43:38 +0300

    Add files via upload

---
+++ OOP_Constructor_ders-6/models/bebek.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Constructor_ders_6.models
+{
+    public class bebek
+    {
+
+        //bir sınıftan nesnelerin örneklerini oluştururken kullanılır.
+        public bebek()
+        {
+            BirthDate = DateTime.Now;
+        }
+
+
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+
+
+    }
+
+
+   
+
+    
+}
